fcb024d2cb8aa6b1ba3fa7496c2b47651ecf2f72
Liberhome <46431509+Liberhome@users.noreply.github.com>
2020-03-07 13:09:22 +0800

    Delete Client.java

--- Client.java
+++
@@ -1,9 +0,0 @@
-package Way2;
-
-public class Client {
-    public static void main(String[] args) {
-        Chart chart;
-        chart=ChartFactory.getChart("pie");
-        chart.display();
-    }
-}
